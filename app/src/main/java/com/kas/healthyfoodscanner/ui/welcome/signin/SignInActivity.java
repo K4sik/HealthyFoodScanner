0cf5ca3e8455b325c88a14a99995d217d99f6bc6
K4sik <kasiks597@gmail.com>
2022-11-21 22:55:52 +0200

    Added SignIn page

---
+++ app/src/main/java/com/kas/healthyfoodscanner/ui/welcome/signin/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.kas.healthyfoodscanner.ui.welcome.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kas.healthyfoodscanner.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
