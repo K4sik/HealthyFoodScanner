2b691e6e5a6b97721133d60b98225bb164508d43
K4sik <kasiks597@gmail.com>
2022-11-21 22:54:02 +0200

    Added LogIn page

---
+++ app/src/main/java/com/kas/healthyfoodscanner/ui/welcome/login/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.kas.healthyfoodscanner.ui.welcome.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kas.healthyfoodscanner.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
