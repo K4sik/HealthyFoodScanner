92e039c8275696202d7ca6c24a7b26f844ec8e1a
K4sik <kasiks597@gmail.com>
2022-11-22 14:39:43 +0200

    Added Sign Up page

--- app/src/main/java/com/kas/healthyfoodscanner/ui/welcome/signup/SignUpActivity.java
+++ app/src/main/java/com/kas/healthyfoodscanner/ui/welcome/signup/SignUpActivity.java
@@ -2,15 +2,37 @@ package com.kas.healthyfoodscanner.ui.welcome.signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.kas.healthyfoodscanner.R;
+import com.kas.healthyfoodscanner.ui.welcome.login.LogInActivity;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextInputEditText username;
+
+    private TextInputEditText password;
+
+    private TextInputEditText confirmPassword;
+
+    private Button signUpBtn;
+
+    private TextView loginTv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        loginTv = findViewById(R.id.log_in_tv);
+        loginTv.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), LogInActivity.class));
+        });
+
     }
 }
