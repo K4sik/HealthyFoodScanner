54abd4af5dd3cb91a456f14e96f4de71721e7dfb
K4sik <kasiks597@gmail.com>
2022-11-23 12:20:21 +0200

    Fixed layout

---
+++ app/src/main/java/com/kas/healthyfoodscanner/ui/home/scanner/ResultScanActivity.java
@@ -0,0 +1,14 @@
+package com.kas.healthyfoodscanner.ui.home.scanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_scan);
+    }
+}
