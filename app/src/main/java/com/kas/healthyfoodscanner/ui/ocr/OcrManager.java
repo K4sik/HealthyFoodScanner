1d1083b9976220ce29ccc1b5dd018805840566db
K4sik <kasiks597@gmail.com>
2023-10-09 17:48:50 +0300

    Added Tesseract OCR

--- app/src/main/java/com/kas/healthyfoodscanner/ui/ocr/OcrManager.java
+++ app/src/main/java/com/kas/healthyfoodscanner/ui/ocr/OcrManager.java
@@ -1,2 +1,33 @@
-package com.kas.healthyfoodscanner.ui.ocr;public class OcrManager {
+package com.kas.healthyfoodscanner.ui.ocr;
+
+import android.graphics.Bitmap;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+import com.kas.healthyfoodscanner.ui.product.AddProductActivity;
+
+public class OcrManager {
+    TessBaseAPI baseAPI = null;
+    public void initAPI()
+    {
+        baseAPI = new TessBaseAPI();
+        // after copy, my path to trainned data is getExternalFilesDir(null)+"/tessdata/"+"vie.traineddata";
+        // but init() function just need parent folder path of "tessdata", so it is getExternalFilesDir(null)
+        String dataPath = AddProductActivity.instance.getTessDataParentDirectory();
+        baseAPI.init(dataPath,"ukr");
+        // language code is name of trainned data file, except extendsion part
+        // "vie.traineddata" => language code is "vie"
+
+        // first param is datapath which is  part to the your trainned data, second is language code
+        // now, your trainned data stored in assets folder, we need to copy it to another external storage folder.
+        // It is better do this work when application start firt time
+    }
+
+    public String startRecognize(Bitmap bitmap)
+    {
+        if(baseAPI ==null)
+            initAPI();
+        baseAPI.setImage(bitmap);
+        return baseAPI.getUTF8Text();
+    }
+
 }
--- app/src/main/java/com/kas/healthyfoodscanner/ui/product/AddProductActivity.java
+++ app/src/main/java/com/kas/healthyfoodscanner/ui/product/AddProductActivity.java
@@ -3,16 +3,21 @@ package com.kas.healthyfoodscanner.ui.product;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -25,14 +30,20 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.googlecode.tesseract.android.TessBaseAPI;
 import com.kas.healthyfoodscanner.MainActivity;
 import com.kas.healthyfoodscanner.R;
 import com.kas.healthyfoodscanner.ui.database.DatabaseHelper;
 import com.kas.healthyfoodscanner.ui.home.scanner.BarcodeScannerActivity;
+import com.kas.healthyfoodscanner.ui.ocr.OcrManager;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class AddProductActivity extends AppCompatActivity {
 
@@ -74,10 +85,28 @@ public class AddProductActivity extends AppCompatActivity {
 
     private Bitmap bitmap;
 
+    public static AddProductActivity instance = null;
+
+    public static final String TESS_DATA = "/tessdata/";
+    private static final String TAG = AddProductActivity.class.getSimpleName();
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/Tess/";
+    private TextView textView;
+    private TessBaseAPI tessBaseAPI;
+    private Uri outputFileDir;
+    private String mCurrentPhotoPath;
+
+    OcrManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        instance = this;
+        copyTessDataForTextRecognizer();
+        manager = new OcrManager();
+
+//        prepareTessData();
 
         imageViewResult = findViewById(R.id.image_view);
 
@@ -241,6 +270,11 @@ public class AddProductActivity extends AppCompatActivity {
                 Uri resultUri = result.getUri();
                 try {
                     bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+//                    prepareTessData();
+//                    startOCR(bitmap);
+
+                    manager.initAPI();
+
                     getTextFromImage(bitmap);
                     imageViewResult.setImageBitmap(bitmap);
                 } catch (IOException e) {
@@ -262,16 +296,145 @@ public class AddProductActivity extends AppCompatActivity {
         } else {
             Frame frame = new Frame.Builder().setBitmap(bitmap).build();
             SparseArray<TextBlock> textBlockSparseArray = textRecognizer.detect(frame);
-            StringBuilder stringBuilder = new StringBuilder();
 
-            for (int i = 0; i < textBlockSparseArray.size(); i++){
-                TextBlock textBlock = textBlockSparseArray.valueAt(i);
-                stringBuilder.append(textBlock.getValue());
-                stringBuilder.append("\n");
-            }
+            StringBuilder stringBuilder = new StringBuilder();
+            String str = manager.startRecognize(bitmap);
+
+//            String str = getText(bitmap);
+//            String str = startOCR(bitmap);
+
+//            stringBuilder.append(str);
+//            for (int i = 0; i < textBlockSparseArray.size(); i++){
+//                TextBlock textBlock = textBlockSparseArray.valueAt(i);
+//                stringBuilder.append(textBlock.getValue());
+//                stringBuilder.append("\n");
+//            }
             textInputEditTextDescription.setText(stringBuilder.toString());
+            textInputEditTextDescription.setText(str);
             buttonIngredientsCapture.setText("Retake");
             buttonSave.setVisibility(View.VISIBLE);
         }
     }
+
+    private void copyTessDataForTextRecognizer() {
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                AssetManager assetManager = AddProductActivity.instance.getAssets();
+                OutputStream outputStream = null;
+                try {
+                    InputStream inputStream = assetManager.open("ukr.traineddata");
+                    String tesspath = instance.tessDataPath();
+                    File tessFolder = new File(tesspath);
+                    if(!tessFolder.exists())
+                        tessFolder.mkdir();
+                    String tessData = tesspath+"/"+"ukr.traineddata";
+                    File tessFile = new File(tessData);
+                    if(!tessFile.exists())
+                    {
+                        outputStream = new FileOutputStream(tessData);
+                        byte[] buffer = new byte[1024];
+                        int read = inputStream.read(buffer);
+                        while (read != -1) {
+                            outputStream.write(buffer, 0, read);
+                            read = inputStream.read(buffer);
+                        }
+                        Log.d("AddProductActivity", " Did finish copy tess file  ");
+                    }
+                    else
+                        Log.d("AddProductActivity", " tess file exist  ");
+
+                } catch (Exception e)
+                {
+                    Log.d("AddProductActivity", "couldn't copy with the following error : "+e.toString());
+                }finally {
+                    try {
+                        if(outputStream!=null)
+                            outputStream.close();
+                    }catch (Exception exx)
+                    {
+
+                    }
+                }
+            }
+        };
+        new Thread(run).start();
+
+    }
+
+    private String tessDataPath() {
+        return AddProductActivity.instance.getExternalFilesDir(null)+"/tessdata/";
+    }
+
+    public String getTessDataParentDirectory() {
+        return AddProductActivity.instance.getExternalFilesDir(null).getAbsolutePath();
+    }
+
+    //doesn't work properly
+
+//    private String getText(Bitmap bitmap){
+//        try{
+//            tessBaseAPI = new TessBaseAPI();
+//        }catch (Exception e){
+//            Log.e(TAG, e.getMessage());
+//        }
+//        tessBaseAPI.init(DATA_PATH,"ukr");
+//        tessBaseAPI.setImage(bitmap);
+//        String retStr = "No result";
+//        try{
+//            retStr = tessBaseAPI.getUTF8Text();
+//        }catch (Exception e){
+//            Log.e(TAG, e.getMessage());
+//        }
+//        tessBaseAPI.end();
+//        return retStr;
+//    }
+
+//    private void prepareTessData()
+//    {
+//        try{
+//            File dir = getExternalFilesDir(TESS_DATA);
+//            if(!dir.exists()){
+//                if (!dir.mkdir()) {
+//                    Toast.makeText(getApplicationContext(), "The folder " + dir.getPath() + "was not created", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//            String fileList[] = getAssets().list("");
+//            for(String fileName : fileList){
+//                String pathToDataFile = dir + "/" + fileName;
+//                int ino=fileName.lastIndexOf(".");
+//                String extension = (ino>=0)?fileName.substring(ino):"";
+//                if((new File(pathToDataFile)).exists() && extension.equals(".traineddata")){
+//                    InputStream in = getAssets().open(fileName);
+//                    OutputStream out = new FileOutputStream(pathToDataFile);
+//                    byte [] buff = new byte[1024];
+//                    int len ;
+//                    while(( len = in.read(buff)) > 0){
+//                        out.write(buff,0,len);
+//                    }
+//                    in.close();
+//                    out.close();
+//                }
+//            }
+//        } catch (Exception e) {
+//            Toast.makeText(getApplicationContext(), "Error :"+e.getMessage(), Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    private String startOCR(Bitmap bitmap){
+//        String result = "";
+//        try{
+//            BitmapFactory.Options options = new BitmapFactory.Options();
+//            options.inJustDecodeBounds = false;
+//            options.inSampleSize = 6;
+////            Bitmap bitmap2 = BitmapFactory.decodeFile(bitmap, options);
+//            result = getText(bitmap);
+//            return result;
+////            textInputEditTextDescription.setText(result);
+//        }catch (Exception e){
+//            Log.e(TAG, e.getMessage());
+//        }
+//        return result;
+//    }
+
 }
